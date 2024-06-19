264486fa1e4d7d8e39ef6edf49345ea71d8a368c
Virginia Gutiérrez <virginiagutierrez1986@gmail.com>
2024-06-19 18:08:40 -0300

    Update conexiondb.php

--- config/conexiondb.php
+++ config/conexiondb.php
@@ -4,13 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Define las credenciales de la base de datos
+//credenciales de la base de datos
 define('DB_HOST', 'localhost');
 define('DB_USER', 'USUARIO');
 define('DB_PASS', 'PASS');
 define('DB_NAME', 'afili');
 
-// Intenta establecer la conexión a la base de datos
+//establecer la conexión a la base de datos
 $conexion_db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 // Establece el conjunto de caracteres a UTF-8
 $conexion_db->set_charset("utf8");
